607eab633d885cbfd6bd25f4315c47e9603cb7fb
Abdul Rafay <abdulrafay88243@gmail.com>
2026-02-20 18:08:58 +0500

    Locate executable files

--- app/main.py
+++ app/main.py
@@ -1,28 +1,31 @@
 import sys
+import shutil
+import os
 
-BUILTIN ={"exit","echo","type"}
+BUILTIN = {"exit", "echo", "type"}
 
 def main():
-    # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input().strip()
+        command = input().strip()  
         if not command:
             continue
-
-        if command.startswith("echo"):
-            print(command[5:])
+        if command.startswith("echo "):
+            print(command[5:])      
         elif command.startswith("exit"):
-            break
+            break     
         elif command.startswith("type "):
             target = command[5:]
             if target in BUILTIN:
-                print(f"{command[5:]} is a shell builtin")
+                print(f"{target} is a shell builtin")
             else:
-                print(f"{command[5:]}: not found")
+                found_path = shutil.which(target)
+                if found_path:
+                    print(f"{target} is {found_path}")
+                else:
+                    print(f"{target}: not found")               
         else:
             print(f"{command}: command not found")
-
 
 if __name__ == "__main__":
     main()
