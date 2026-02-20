277279beeb267340ccb861cd13f083b24652630f
Abdul Rafay <abdulrafay88243@gmail.com>
2026-02-20 17:51:13 +0500

    Implement Type

--- app/main.py
+++ app/main.py
@@ -1,15 +1,25 @@
 import sys
 
+BUILTIN ={"exit","echo","type"}
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        if command[0:4] == "echo":
+        command = input().strip()
+        if not command:
+            continue
+
+        if command.startswith("echo"):
             print(command[5:])
-        elif command == "exit":
+        elif command.startswith("exit"):
             break
+        elif command.startswith("type "):
+            target = command[5:]
+            if target in BUILTIN:
+                print(f"{command[5:]} is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         else:
             print(f"{command}: command not found")
 
